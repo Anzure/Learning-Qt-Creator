2696b632d787828baa29787d6e401a6fac07084a
André Mathisen <9752808+anzure@users.noreply.github.com>
2023-04-16 14:17:34 +0200

    💬 Button popup and logging

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,12 +7,18 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    qInfo() << "Constructed main window";
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    qInfo() << "Destructed main window";
     delete ui;
 }
 
-
+void MainWindow::on_pushButton_clicked()
+{
+    qInfo() << "Clicked button";
+    QMessageBox::information(this, "Informasjon", "Du trykket");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 
+#include <QMessageBox>
 
 
 QT_BEGIN_NAMESPACE
@@ -17,6 +18,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
